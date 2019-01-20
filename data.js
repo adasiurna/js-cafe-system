89ef9c7a319a94e62e233376a1d6a93f29ab7fba
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2019-01-21 00:16:55 +0200

    order is formed and rendered

--- data.js
+++ data.js
@@ -1,152 +1,28 @@
 var tables_data = [
-    {
-        tableName: 'table1',
-        ifTaken: '',
-        numberOfGuests: '',
-        guests: {
-            guest1: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest2: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest3: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest4: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest5: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest6: {
-                meal: [],
-                drink: [],
-                dessert: []
-            }
-        }
-    },
-    {
-        tableName: 'table2',
-        ifTaken: '',
-        numberOfGuests: '',
-        guests: {
-            guest1: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest2: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest3: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest4: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest5: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest6: {
-                meal: [],
-                drink: [],
-                dessert: []
-            }
-        }
-    },
-    {
-        tableName: 'table3',
-        ifTaken: '',
-        numberOfGuests: '',
-        guests: {
-            guest1: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest2: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest3: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest4: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest5: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest6: {
-                meal: [],
-                drink: [],
-                dessert: []
-            }
-        }
-    },
-    {
-        tableName: 'table4',
-        ifTaken: '',
-        numberOfGuests: '',
-        guests: {
-            guest1: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest2: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest3: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest4: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest5: {
-                meal: [],
-                drink: [],
-                dessert: []
-            },
-            guest6: {
-                meal: [],
-                drink: [],
-                dessert: []
-            }
-        }
-    }
+  {
+    tableName: 'table1',
+    ifTaken: '',
+    numberOfGuests: '',
+    meals: []
+  },
+  {
+    tableName: 'table2',
+    ifTaken: '',
+    numberOfGuests: '',
+    meals: []
+  },
+  {
+    tableName: 'table3',
+    ifTaken: '',
+    numberOfGuests: '',
+    meals: []
+  },
+  {
+    tableName: 'table4',
+    ifTaken: '',
+    numberOfGuests: '',
+    meals: []
+  }
 
 ]
 
--- main.js
+++ main.js
@@ -6,23 +6,27 @@ $(document).ready(function () {
     console.log('clicked a table');
     let tableIndex = $(this).index();
     let tableName = this.id;
-    $("#showNumberOfGuestsHere").hide();
-    console.log(tableName + ' tableIndex: ' + tableIndex);
 
     // set the active table name to the DOM element with the ID #activeTableName
     $('#activeTableName').html(tableName);
 
-    // check if table is free
-    if (!tables_data[tableIndex].numberOfGuests) {
+    // if table has guests and ordered meals
+    if (tables_data[tableIndex].numberOfGuests && tables_data[tableIndex].meals) {
+      console.log('turim sveciu skaiciu ir turim uzsakytus patiekalus');
+      $('#getNumberOfGuestsForm').show();
+      $('#getNumberOfGuestsForm').html('table ' + tables_data[tableIndex].tableName + ' is taken, has ' + tables_data[tableIndex].numberOfGuests + ' guests');
+      renderGuestOrderForm(tableIndex);
+      renderOrderedMealsList(tableIndex);
+
+    } else {
+      $('#tableIndexInGuestsBox').hide();
+      $("#showNumberOfGuestsHere").hide();
+      $('#guests_box').hide();
+      $('#orderedMealsList').hide();
 
       // if table is free, render form to insert the number of guests
       renderGetNumberOfGuestsForm(tableIndex);
 
-    } else {
-      // if table is taken?
-      // todo
-      $('#getNumberOfGuestsForm').show();
-      $('#getNumberOfGuestsForm').html('table ' + tables_data[tableIndex].tableName + ' is taken, has ' + tables_data[tableIndex].numberOfGuests + ' guests');
     }
 
   });
@@ -41,6 +45,24 @@ $(document).ready(function () {
 
     // renderGuestOrderForm
     renderGuestOrderForm(tableIndex);
+    $('#guests_box').show();
+
+
+
+  })
+
+  $('#guests_box').on('click', '#guestsBoxSubmit', function () {
+    console.log('clicked on the guests box submit button');
+    const tableIndex = $('#guests_box > #tableIndexInGuestsBox').val();
+
+    tables_data[tableIndex].meals.push($('#guests_box > .meal').val());
+    tables_data[tableIndex].meals.push($('#guests_box > .drink').val());
+    tables_data[tableIndex].meals.push($('#guests_box > .dessert').val());
+    const allMeals = tables_data[tableIndex].meals;
+    console.log(allMeals);
+    renderOrderedMealsList(tableIndex);
+    $('#orderedMealsList').show();
+    renderOrderedMealsList(tableIndex);
 
 
   })
@@ -63,7 +85,7 @@ function renderGetNumberOfGuestsForm(tableIndex) {
 
     // if table is free render getNumberOfGuestsForm
     const HTML = '<label for="howMany">How many guests?</label>\
-      <input type="number" name="howMany" id="howMany" min="1" max="6" required>\
+      <input type="number" name="howMany" id="howMany" min="1" max="6" value="2" required>\
       <input type="hidden" id="tableIndexIngetNumberOfGuestsForm" value="'+ tableIndex + '">\
       <input type="button" id="submit" value="Submit"> ';
     $('#getNumberOfGuestsForm').html(HTML);
@@ -75,42 +97,44 @@ function renderGetNumberOfGuestsForm(tableIndex) {
 function renderGuestOrderForm(tableIndex) {
   console.log('tables_data[tableIndex].numberOfGuests renderGuestOrderForm funkcijoje: ' + tables_data[tableIndex].numberOfGuests);
 
-  var numberOfGuests = tables_data[tableIndex].numberOfGuests;
-  var HTML = '';
-  for (var i = 0; i < numberOfGuests; i++) {
-    HTML += '<div class="guest">\
-        <form class="meal">\
-            Meal:\
-            <select name="meal">\
-                        <option value="fish">Fish</option>\
-                        <option value="meat">Meat</option>\
-                        <option value="soup">Soup</option>\
-                        <option value="sallad">Sallad</option>\
+  var HTML = '<input id="tableIndexInGuestsBox" value="' + tableIndex + '"><hr>\
+        Meal: \
+          <select class="meal" name="meal">\
+                        <option></option>\
+                        <option>Fish 8.49</option>\
+                        <option>Meat 7.89</option>\
+                        <option>Soup 2.20</option>\
+                        <option>Sallad 5.50</option>\
+                    </select><br>\
+        Drink:\
+            <select class="drink" name="drink">\
+                        <option></option>\
+                        <option>Water 1.50</option>\
+                        <option>Sparkling water 1.50</option>\
+                        <option>Tea 2.50</option>\
+                        <option>Frech juice 2.80</option>\
+                    </select><br>\
+        Dessert:\
+            <select class="dessert" name="dessert">\
+                        <option></option>\
+                        <option>Carrot cake 5.50</option>\
+                        <option>Ice cream 4.20</option>\
+                        <option>Creme brulle 4.50</option>\
+                        <option>Blueberry pie 5.90</option>\
                     </select>\
-                    <input type="button" value="Submit">\
-                </form>\
-        <form class="drink">\
-            Drink:\
-            <select name="drink">\
-                        <option value="water">Water</option>\
-                        <option value="sparkling_water">Sparkling water</option>\
-                        <option value="tea">Tea</option>\
-                        <option value="fresh_juice">Frech juice</option>\
-                    </select>\
-                    <input type="button" value="Submit">\
-                </form>\
-        <form class="dessert">\
-            Dessert:\
-            <select name="dessert">\
-                        <option value="carrot_cake">Carrot cake</option>\
-                        <option value="ice_cream">Ice cream</option>\
-                        <option value="creme_brulle">Creme brulle</option>\
-                        <option value="blueberry_pie">Blueberry pie</option>\
-                    </select>\
-                    <input type="button" value="Submit">\
-                </form>\
-    </div ><hr>'
-
-  }
+    <hr>\
+  <input type="button" id="guestsBoxSubmit" value="Submit">'
   $("#guests_box").html(HTML);
+}
+
+function renderOrderedMealsList(tableIndex) {
+  const allMeals = tables_data[tableIndex].meals;
+  let HTML = '<ol>';
+  for (var i = 0; i < allMeals.length; i++) {
+    if (allMeals[i]) {
+      HTML += '<li>' + allMeals[i] + '</li>';
+    }
+  }
+  HTML += '</ol>';
+  $('#orderedMealsList').html(HTML);
 }
